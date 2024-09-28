4aa87384fc0bdc9f41b83ba51118fe9ddbb34100
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-09-28 16:41:55 +0700

    Define routes

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/features/app/splash_screen/splash_screen.dart';
+import 'package:flutter_application_1/features/user_auth/presentation/pages/home_page.dart';
 import 'package:flutter_application_1/features/user_auth/presentation/pages/login_page.dart';
+import 'package:flutter_application_1/features/user_auth/presentation/pages/setting_page.dart';
+import 'package:flutter_application_1/features/user_auth/presentation/pages/sign_up_page.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,12 +33,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: SplashScreen(
-        child: LoginPage(),
-      ),
+      routes: {
+        '/': (context) => const SplashScreen(
+              // Here, you can decide whether to show the LoginPage or HomePage based on user authentication
+              child: LoginPage(),
+            ),
+        '/login': (context) => const LoginPage(),
+        '/signUp': (context) => const SignUpPage(),
+        '/home': (context) => const HomePage(),
+        '/setting': (context) => const SettingPage(),
+      },
     );
   }
 }
