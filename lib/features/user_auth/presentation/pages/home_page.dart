fe2758fb7959ed0a2d9671cffc74417c500e8a70
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-09-28 16:41:39 +0700

    Home page

--- lib/features/user_auth/presentation/pages/home_page.dart
+++ lib/features/user_auth/presentation/pages/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/global/common/toast.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,9 +8,40 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("HomePage")),
-      body: const Center(
-        child: Text("Welcome To Home"),
+      appBar: AppBar(title: const Text("HomePage")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Center(
+            child: Text("Welcome Home!",
+                style: TextStyle(fontWeight: FontWeight.bold)),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          GestureDetector(
+            onTap: () {
+              FirebaseAuth.instance.signOut();
+              Navigator.pushReplacementNamed(context, "/login");
+              showToast(message: "Successfully signed out");
+            },
+            child: Container(
+              height: 45,
+              width: 100,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(10)),
+              child: const Center(
+                child: Text(
+                  "Sign out",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
