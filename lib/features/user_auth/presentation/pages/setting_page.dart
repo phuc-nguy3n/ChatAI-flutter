d8414b6a04efe471f31e607b88b5df36c68c93f3
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-09-30 15:12:17 +0700

    Updating user name

--- lib/features/user_auth/presentation/pages/setting_page.dart
+++ lib/features/user_auth/presentation/pages/setting_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/features/repositories/firebase_repository.dart';
 import 'package:flutter_application_1/features/user_auth/presentation/widgets/form_container_widget.dart';
+import 'package:flutter_application_1/global/common/toast.dart';
+import 'package:flutter_application_1/global/provider/user_provider.dart';
+import 'package:get/get.dart';
 
 class SettingPage extends StatefulWidget {
   const SettingPage({super.key});
@@ -9,6 +13,19 @@ class SettingPage extends StatefulWidget {
 }
 
 class _SettingPageState extends State<SettingPage> {
+  bool _isUpdating = false;
+
+  final TextEditingController _userNameController = TextEditingController();
+  final UserController _userController = Get.find<UserController>();
+  final FirebaseRepository _firebaseRepository = FirebaseRepository();
+
+  @override
+  void dispose() {
+    _userNameController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,21 +35,60 @@ class _SettingPageState extends State<SettingPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const FormContainerWidget(
+            FormContainerWidget(
+              controller: _userNameController,
               hintText: "Username",
-              isPasswordField: true,
+              isPasswordField: false,
             ),
             const SizedBox(height: 20),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text("Cập nhật"),
+            GestureDetector(
+              onTap: () {
+                _updateUserName();
+              },
+              child: Container(
+                width: double.infinity,
+                height: 45,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: _isUpdating
+                      ? const CircularProgressIndicator(
+                          color: Colors.white,
+                        )
+                      : const Text(
+                          "Update",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                ),
               ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  void _updateUserName() async {
+    String uid = _userController.user.value!.id;
+    String username = _userNameController.text;
+
+    if (username == '') {
+      showToast(message: "Please enter your name");
+    } else {
+      setState(() {
+        _isUpdating = true;
+      });
+      await _firebaseRepository.updateUserName(uid, username);
+      setState(() {
+        _isUpdating = false;
+      });
+      // ignore: use_build_context_synchronously
+      Navigator.pushReplacementNamed(context, "/home");
+    }
   }
 }
